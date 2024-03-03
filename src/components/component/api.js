39185fac5100e4967ad3d7f51b25ee6dd398a088
mansourkira <mansour.benmansour@esprit.tn>
2024-03-03 11:52:48 +0100

    phase 1 : questions , dark mode , next , preveuis buttons

---
+++ src/components/component/api.js
@@ -0,0 +1,54 @@
+let api = [
+  {
+    Question: "Quel est votre genre?",
+    Options: ["Masculin", "Féminin"],
+  },
+  {
+    Question: "Etes-vous venu le:",
+    Options: ["Matin", "Le soir", "La nuit", "Le weekend", "Jour ouvrable"],
+  },
+  {
+    Question: "Dans quelle tranche d’âge appartenez-vous?",
+    Options: ["15-24", "25-39", "40-54", "55-69", "70+"],
+  },
+  {
+    Question: "Vous consultez les urgences :",
+    Options: ["Pour la 1ére fois", "Pour la (…) fois"],
+  },
+  {
+    Question: "Votre ré-consultation a été dans :",
+    Options: [
+      "Moins de 7 jours",
+      "Entre 7 jours et 1 mois",
+      "Entre 1 et 6 mois",
+      "Plus que 6 mois",
+    ],
+  },
+  {
+    Question: "Vous êtes orienté vers le service d’urgences par :",
+    Options: [
+      "A la demande de votre médecin traitant",
+      "A votre propre initiative",
+      "Par un autre professionnel de santé",
+      "Par un service mobile d’urgence",
+    ],
+  },
+  {
+    Question: "Vous avez été transporté par :",
+    Options: [
+      "Les pompiers",
+      "Ambulance (SAMU)",
+      "Vos propres moyens",
+      "Un taxi",
+      "Un ami ou un parent",
+      "A pied",
+    ],
+  },
+  {
+    Question:
+      "Dans le cas de déplacement personnel, les enseignes d'orientation sont-ils bons ?",
+    Options: ["Oui", "Non"],
+  },
+];
+
+export default api;
